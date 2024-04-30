cf3a43c8c2365031a7b7c1c9c670523fc70e0982
FSanchezSillero <150432434+SanchezSillero@users.noreply.github.com>
2024-04-30 14:47:42 +0200

    update

--- src/Entrada.java
+++ src/Entrada.java
@@ -15,7 +15,7 @@ public class Entrada {
         Biblioteca bibliotecaEspTerror = new BibliotecaEspecializada("Biblioteca Halloween", "María José Ruiz", Tipo.terror);//solo aceptara libros de TERROR
         bibliotecaEspTerror.setNombreArchivoCatalogo("bibliotecaEspTerror.obj");
 
-   /*    //creamos algunos libros (no están en ningun catalogo)
+  /*     //creamos algunos libros (no están en ningun catalogo)
         LibroTerror libroTerror1 = new LibroTerror(1, "Frankenstein", "Mary Shelley", 168, Tipo.terror, 14);
         LibroComedia libroComedia1 = new LibroComedia(2, "La vida secreta de Walter Mitty", "James Thurber", 160, Tipo.comedia, TipoHumor.parodia);
         Ensayo ensayo1 = new Ensayo(3, "Los orígenes de la creatividad humana", "Edward O.Wilson", 256, Tipo.ensayo, "Evolución Humana");
